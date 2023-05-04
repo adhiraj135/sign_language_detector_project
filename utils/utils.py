1d695bda6daa99eb15c251272bb9b8c9c5079383
adhiraj135 <adhiraj.singh483@gmail.com>
2023-05-04 05:10:51 -0700

    file added and midified to deploy on web page

---
+++ utils/utils.py
@@ -0,0 +1,16 @@
+import base64
+
+
+class basic_utility_functions:
+
+    def decodeImage(imgstring, fileName):
+       imgdata = base64.b64decode(imgstring)
+       with open("./data/" + fileName, 'wb') as f:
+            f.write(imgdata)
+            f.close()
+
+
+    def encodeImageIntoBase64(croppedImagePath):
+       with open(croppedImagePath, "rb") as f:
+           return base64.b64encode(f.read())
+
